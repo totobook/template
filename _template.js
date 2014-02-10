71e59bb3c1233f845cbbc0799b4bc0148da72f75
totobook <totobook2@gmail.com>
2014-02-10 22:09:05 +0900

    If cannnot parses a string argument, return 0

--- _template.js
+++ _template.js
@@ -1,11 +1,13 @@
 /**
  * The parseDecimal() funtion parses a string argument and returns an integer of the Decimal.
+ * If cannot parses a string argument, return 0.
  * @param  {string} num The value to parse.
  * @return {number} Return Description number.
  */
 function parseDecimal(num)
 {
-	return parseInt(num, 10);
+	var parsed_num = parseInt(num, 10);
+	return isNaN(parsed_num) ? 0 : parsed_num;
 }
 
 /**
